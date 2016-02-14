52022b6917cc8478647b57ae1d4533ed8d3284eb
Mandy Ngo <mango@ucsd.edu>
2016-02-14 11:41:21 -0800

    Added test cases

---
+++ mysamples/function_1.glsl
@@ -0,0 +1,17 @@
+void f(int fe) {
+}
+
+void ff() {
+    f(34);
+}
+
+void g(int a, string b, bool c) {
+
+}
+
+void main() {
+    f(12);
+    ff();
+    g(2, " and ",true);
+    f(10 + 3 % 2 + 5);
+}
--- test-all.sh
+++ test-all.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 python test-all.py
+python test-mysamples.py
