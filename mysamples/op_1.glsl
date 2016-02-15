2623bbd5d67b28b2858a969019a9a313c2681ca1
Mandy Ngo <mango@ucsd.edu>
2016-02-15 14:42:23 -0800

    Added more test

--- mysamples/op_1.glsl
+++ mysamples/op_1.glsl
@@ -1,14 +1,29 @@
 void main() {
-  int a = (1 + 1);
-  int b = 2 +7;
-  int c = 10 - 5;
-  int d = 21/3;
-  int e = 3*456;
-  int f = 82 % 9;
-  int g = - -10;
-  int h = -2;
+  int a;
+  int b;
+  int c;
+  int d;
+  int e;
+  int f;
+  int g;
+  int h;
+  int l;
+  int m;
+  a = (1 + 1);
+  b = -2 + -7;
+  c = 10 - 5;
+  d = +21/-3;
+  e = 3*456;
+  f = 82 / 9;
+  g = - -10;
+  h = -2;
+  l = 2++;
+  m = --l;
   
-  bool i = !true;
-  bool j = false;
-  bool k = (true==false);
+  bool i;
+  bool j;
+  bool k;
+  i = !true;
+  j = false;
+  k = (true==false);
 }
