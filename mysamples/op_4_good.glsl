91cae798fbaa8a989df582311c577d76f93ab7ed
Mandy Ngo <mango@ucsd.edu>
2016-02-15 20:52:48 -0800

    Added more testcases

---
+++ mysamples/op_4_good.glsl
@@ -0,0 +1,25 @@
+void main(){
+    vec4 v4;
+    mat4 m4;
+    vec4 result1;
+    vec4 result2;
+    vec3 v3;
+    vec3 result3;
+    vec3 result4; 
+    float f;
+    mat3 m3;
+    mat3 result5;
+    
+    result1 = m4 * v4;
+    result2 = v4 * m4; 
+    result3 = v3 + f;
+    result4 = f + vec3;
+    result3 = v3 - f;
+    result4 = v3 * f;
+    result3 = f * v3;
+    result4 = v3 / f;
+    result3 = f - v3;
+    result4 = f / v3;
+    result5 = m3 + m3;
+    result6 = v4 + v4;
+}
---
+++ mysamples/op_5_bad.glsl
@@ -0,0 +1,9 @@
+void main(){
+    mat2 m2;
+    vec2 v2;
+    int n;
+    m2 + v2; 
+    m2 / v2; 
+    m2 - v2;
+    n + m3;
+}
