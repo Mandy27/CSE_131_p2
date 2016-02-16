650679c4628d5c8f7b65c621aba62f81f11a411a
Mandy Ngo <mango@ucsd.edu>
2016-02-15 22:38:35 -0800

    more tests

---
+++ mysamples/test_3.glsl
@@ -0,0 +1,29 @@
+void main() {
+
+    int a;
+
+    switch(a--) {
+        case 1:
+            a = 5 + a--;
+            break;
+        case 2:
+            a--;
+        case 3:
+        {
+            int a;
+            a = 10;
+            b = 5;
+            {
+               float a;
+               a = 7;
+               a = 8.0;
+            }
+         }
+        break;
+        break;
+        default:
+
+    }
+
+
+}
